02ff30df5dd2ff619f98e9a1548a95147cad9ac5
Sergiu Groza <serioja90@gmail.com>
2014-09-18 00:22:01 +0200

    Implementata la funzionalità che abilita/disabilita i bottoncini dei livelli in base ad un numero intero (che in futuro sarà il livello)

--- PhoneApp2/menuSceltaLivello.xaml.cs
+++ PhoneApp2/menuSceltaLivello.xaml.cs
@@ -7,11 +7,27 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Media;
 
 namespace PhoneApp2{
   public partial class menuSceltaLivello : PhoneApplicationPage{
+    SolidColorBrush activeColor;
+    SolidColorBrush lockedColor;
+    int greatestLevel;
     public menuSceltaLivello(){
-        InitializeComponent();
+      InitializeComponent();
+      greatestLevel = 1;
+      activeColor = new SolidColorBrush(new Color() { A = 0x99, R = 0x21, G = 0xE2, B = 0x18 });
+      lockedColor = new SolidColorBrush(new Color() { A = 0x99, R = 0xEA, G = 0x00, B = 0x00 });
+      foreach(Button button in ContentPanel.Children){
+        if (Int16.Parse(button.Tag.ToString()) <= greatestLevel) {
+          button.Background = activeColor;
+          button.IsHitTestVisible = true;
+        } else {
+          button.Background = lockedColor;
+          button.IsHitTestVisible = false;
+        }
+      }
     }
     private void StartLevel(object sender, RoutedEventArgs e) {
       String tag = (sender as Button).Tag.ToString();
